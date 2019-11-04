584061098896761868a4f2da943d742c97d2e815
Elliot <16152037+LluisMas@users.noreply.github.com>
2019-11-04 18:27:29 +0100

    accel

--- activity3/activity3.ino
+++ activity3/activity3.ino
@@ -1,4 +1,5 @@
 #include "DHT.h"
+#include <Wire.h>
  
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
@@ -11,6 +12,19 @@
 // Connect pin 2 of the sensor to whatever your DHTPIN is
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+byte _buff[6];
+
+const int DEVICE_ADDRESS = (0x53);  
+
+char POWER_CTL = 0x2D;
+char DATA_FORMAT = 0x31;
+char DATAX0 = 0x32;  //X-Axis Data 0
+char DATAX1 = 0x33; //X-Axis Data 1
+char DATAY0 = 0x34; //Y-Axis Data 0
+char DATAY1 = 0x35; //Y-Axis Data 1
+char DATAZ0 = 0x36; //Z-Axis Data 0
+char DATAZ1 = 0x37; //Z-Axis Data 1
  
 const int DHTPin = 5;     // what digital pin we're connected to
  
@@ -24,6 +38,10 @@ void setup() {
    pinMode(EchoPin, INPUT);
    Serial.println("DHTxx test!");
    dht.begin();
+
+   Wire.begin();
+   writeTo(DEVICE_ADDRESS, DATA_FORMAT, 0x01); //Poner ADXL345 en +- 4G
+   writeTo(DEVICE_ADDRESS, POWER_CTL, 0x08);  //Poner el ADXL345 
 }
  
 void loop() {
@@ -36,6 +54,8 @@ void loop() {
    // Reading temperature or humidity takes about 250 milliseconds!
    float h = dht.readHumidity();
    float t = dht.readTemperature();
+   readAccel(); //Leer aceleracion x, y, z
+   delay(500);
 
    if (isnan(h) || isnan(t)) {
       Serial.println("Failed to read from DHT sensor!");
@@ -63,4 +83,48 @@ int ping(int TriggerPin, int EchoPin) {
    
    distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
    return distanceCm;
+}
+
+void readAccel() {
+  //Leer los datos
+  uint8_t numBytesToRead = 6;
+  readFrom(DEVICE_ADDRESS, DATAX0, numBytesToRead, _buff);
+
+  //Leer los valores del registro y convertir a int (Cada eje tiene 10 bits, en 2 Bytes LSB)
+  int x = (((int)_buff[1]) << 8) | _buff[0];   
+  int y = (((int)_buff[3]) << 8) | _buff[2];
+  int z = (((int)_buff[5]) << 8) | _buff[4];
+  Serial.print("x: ");
+  Serial.print( x );
+  Serial.print(" y: ");
+  Serial.print( y );
+  Serial.print(" z: ");
+  Serial.println( z );
+}
+
+
+void writeTo(int device, byte address, byte val) 
+{
+  Wire.beginTransmission(device);
+  Wire.write(address);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+void readFrom(int device, byte address, int num, byte buff[])
+{
+  Wire.beginTransmission(device);
+  Wire.write(address);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(device);
+  Wire.requestFrom(device, num);
+
+  int i = 0;
+  while(Wire.available())
+  {
+    buff[i++] = Wire.read();  
+  }
+  
+  Wire.endTransmission();
 }
